2603796c562e4f1fa13e56e61f46ff93a9724eb5
Maxim Kolomeychenko <maxim@supervise.ly>
2022-05-22 16:45:09 +0300

    debug starts

--- prepare_venv.sh
+++ prepare_venv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -d "venv" ]; then
+if [ -d ".venv" ]; then
     echo "VENV already exists, will be removed"
-    rm -rf venv
+    rm -rf .venv
 fi
 
 echo "VENV will be created" && \
-python3 -m venv venv && \
-source venv/bin/activate && \
+python3 -m venv .venv && \
+source .venv/bin/activate && \
 echo "Install requirements..." && \
 pip3 install -r requirements.txt && \
 echo "Requirements have been successfully installed" && \
--- project-dataset/src/main.py
+++ project-dataset/src/main.py
@@ -14,9 +14,9 @@ from shared_utils.connect import get_model_info
 from shared_utils.inference import postprocess
 import init_ui as ui
 
-owner_id = int(os.environ['context.userId'])
-team_id = int(os.environ['context.teamId'])
-workspace_id = int(os.environ['context.workspaceId'])
+owner_id = int(os.environ["context.userId"])
+team_id = int(os.environ["context.teamId"])
+workspace_id = int(os.environ["context.workspaceId"])
 
 project_id = None
 dataset_id = os.environ.get("modal.state.slyDatasetId")
@@ -36,19 +36,15 @@ project_meta: sly.ProjectMeta = None
 
 image_grid_options = {
     "opacity": 0.3,
-    "fillRectangle": True, #False,
+    "fillRectangle": True,  # False,
     "enableZoom": True,
     "syncViews": True,
     "showPreview": True,
-    "selectable": False
+    "selectable": False,
 }
 
 empty_gallery = {
-    "content": {
-        "projectMeta": {},
-        "annotations": {},
-        "layout": []
-    },
+    "content": {"projectMeta": {}, "annotations": {}, "layout": []},
     "options": image_grid_options,
 }
 
@@ -110,11 +106,15 @@ def preview(api: sly.Api, task_id, context, state, app_logger):
         inf_setting = yaml.safe_load(state["settings"])
     except Exception as e:
         inf_setting = {}
-        app_logger.warn(f'Model Inference launched without additional settings. \n'
-                        f'Reason: {e}', exc_info=True)
+        app_logger.warn(
+            f"Model Inference launched without additional settings. \n" f"Reason: {e}",
+            exc_info=True,
+        )
 
     image_info = random.choice(input_images)
-    input_ann, res_ann, res_project_meta = apply_model_to_image(api, state, image_info.dataset_id, image_info.id,  inf_setting)
+    input_ann, res_ann, res_project_meta = apply_model_to_image(
+        api, state, image_info.dataset_id, image_info.id, inf_setting
+    )
 
     preview_gallery = {
         "content": {
@@ -125,32 +125,36 @@ def preview(api: sly.Api, task_id, context, state, app_logger):
                     "figures": [label.to_json() for label in input_ann.labels],
                     "info": {
                         "title": "input",
-                    }
+                    },
                 },
                 "output": {
                     "url": image_info.full_storage_url,
                     "figures": [label.to_json() for label in res_ann.labels],
                     "info": {
                         "title": "output",
-                    }
-                }
+                    },
+                },
             },
-            "layout": [["input"], ["output"]]
+            "layout": [["input"], ["output"]],
         },
         "options": image_grid_options,
     }
 
     fields = [
         {"field": "state.processing", "payload": False},
-        {"field": "data.gallery", "payload": preview_gallery}
+        {"field": "data.gallery", "payload": preview_gallery},
     ]
     api.task.set_fields(task_id, fields)
 
 
 def apply_model_to_image(api, state, dataset_id, image_id, inf_setting):
-    orig_anns, res_anns, res_project_meta = apply_model_to_images(api, state, dataset_id, [image_id], inf_setting)
+    orig_anns, res_anns, res_project_meta = apply_model_to_images(
+        api, state, dataset_id, [image_id], inf_setting
+    )
     if orig_anns is None:
-        orig_ann = sly.Annotation.from_json(api.annotation.download(image_id).annotation, project_meta)
+        orig_ann = sly.Annotation.from_json(
+            api.annotation.download(image_id).annotation, project_meta
+        )
     else:
         orig_ann = orig_anns[0]
     return orig_ann, res_anns[0], res_project_meta
@@ -159,24 +163,30 @@ def apply_model_to_image(api, state, dataset_id, image_id, inf_setting):
 def apply_model_to_images(api, state, dataset_id, ids, inf_setting):
     nn_session_id = state["sessionId"]
     add_mode = state["addMode"]
-    ann_pred_json = api.task.send_request(nn_session_id, "inference_batch_ids",
-                                          data={
-                                              "dataset_id": dataset_id,
-                                              "batch_ids": ids,
-                                              "settings": inf_setting
-                                          })
-    ann_preds = [sly.Annotation.from_json(pred_json, model_meta) for pred_json in ann_pred_json]
+    ann_pred_json = api.task.send_request(
+        nn_session_id,
+        "inference_batch_ids",
+        data={"dataset_id": dataset_id, "batch_ids": ids, "settings": inf_setting},
+    )
+    ann_preds = [
+        sly.Annotation.from_json(pred_json, model_meta) for pred_json in ann_pred_json
+    ]
 
     res_project_meta = project_meta.clone()
     res_anns = []
     for ann_pred in ann_preds:
-        res_ann, res_project_meta = postprocess(api, project_id, ann_pred, res_project_meta, model_meta, state)
+        res_ann, res_project_meta = postprocess(
+            api, project_id, ann_pred, res_project_meta, model_meta, state
+        )
         res_anns.append(res_ann)
 
     original_anns = None
     if add_mode == "merge":
         original_anns = api.annotation.download_batch(dataset_id, ids)
-        original_anns = [sly.Annotation.from_json(ann_info.annotation, project_meta) for ann_info in original_anns]
+        original_anns = [
+            sly.Annotation.from_json(ann_info.annotation, project_meta)
+            for ann_info in original_anns
+        ]
 
         merged_anns = []
         for ann, pred in zip(original_anns, res_anns):
@@ -192,7 +202,10 @@ def apply_model_to_images(api, state, dataset_id, ids, inf_setting):
 def apply_model(api: sly.Api, task_id, context, state, app_logger):
     def _update_progress(progress):
         fields = [
-            {"field": "data.progress", "payload": int(progress.current * 100 / progress.total)},
+            {
+                "field": "data.progress",
+                "payload": int(progress.current * 100 / progress.total),
+            },
             {"field": "data.progressCurrent", "payload": progress.current},
             {"field": "data.progressTotal", "payload": progress.total},
         ]
@@ -202,18 +215,24 @@ def apply_model(api: sly.Api, task_id, context, state, app_logger):
         inf_setting = yaml.safe_load(state["settings"])
     except Exception as e:
         inf_setting = {}
-        app_logger.warn(f'Model Inference launched without additional settings. \n'
-                        f'Reason: {e}', exc_info=True)
+        app_logger.warn(
+            f"Model Inference launched without additional settings. \n" f"Reason: {e}",
+            exc_info=True,
+        )
 
     global project_meta
     res_project_meta = project_meta.clone()
-    res_project = api.project.create(workspace_id, state["resProjectName"], change_name_if_conflict=True)
+    res_project = api.project.create(
+        workspace_id, state["resProjectName"], change_name_if_conflict=True
+    )
     api.project.update_meta(res_project.id, res_project_meta.to_json())
 
     progress = sly.Progress("Inference", len(input_images), need_info_log=True)
 
     for dataset in input_datasets:
-        res_dataset = api.dataset.create(res_project.id, dataset.name, dataset.description)
+        res_dataset = api.dataset.create(
+            res_project.id, dataset.name, dataset.description
+        )
         images = api.image.get_list(dataset.id)
 
         for batch in sly.batched(images, batch_size=10):
@@ -223,23 +242,32 @@ def apply_model(api: sly.Api, task_id, context, state, app_logger):
                 image_ids.append(image_info.id)
                 res_names.append(image_info.name)
                 res_metas.append(image_info.meta)
-            _, res_anns, final_project_meta = apply_model_to_images(api, state, dataset.id, image_ids, inf_setting)
+            _, res_anns, final_project_meta = apply_model_to_images(
+                api, state, dataset.id, image_ids, inf_setting
+            )
             if res_project_meta != final_project_meta:
                 res_project_meta = final_project_meta
                 api.project.update_meta(res_project.id, res_project_meta.to_json())
 
-            res_images_infos = api.image.upload_ids(res_dataset.id, res_names, image_ids, metas=res_metas)
+            res_images_infos = api.image.upload_ids(
+                res_dataset.id, res_names, image_ids, metas=res_metas
+            )
             res_ids = [image_info.id for image_info in res_images_infos]
             api.annotation.upload_anns(res_ids, res_anns)
             progress.iters_done_report(len(res_ids))
             if progress.need_report():
                 _update_progress(progress)
 
-    res_project = api.project.get_info_by_id(res_project.id)  # to refresh reference_image_url
+    res_project = api.project.get_info_by_id(
+        res_project.id
+    )  # to refresh reference_image_url
     fields = [
         {"field": "data.resProjectId", "payload": res_project.id},
         {"field": "data.resProjectName", "payload": res_project.name},
-        {"field": "data.resProjectPreviewUrl", "payload": api.image.preview_url(res_project.reference_image_url, 100, 100)},
+        {
+            "field": "data.resProjectPreviewUrl",
+            "payload": api.image.preview_url(res_project.reference_image_url, 100, 100),
+        },
     ]
     api.task.set_fields(task_id, fields)
     api.task.set_output_project(task_id, res_project.id, res_project.name)
@@ -268,17 +296,21 @@ def main():
         input_images.extend(my_app.public_api.image.get_list(ds_info.id))
 
     global project_meta
-    project_meta = sly.ProjectMeta.from_json(my_app.public_api.project.get_meta(project_id))
+    project_meta = sly.ProjectMeta.from_json(
+        my_app.public_api.project.get_meta(project_id)
+    )
 
     ui.init(data, state)
     data["emptyGallery"] = empty_gallery
-    ui.init_input_project(my_app.public_api, data, project_info, len(input_images), dataset_info)
+    ui.init_input_project(
+        my_app.public_api, data, project_info, len(input_images), dataset_info
+    )
     state["resProjectName"] = project_info.name + " (inf)"
     ui.init_output_project(data)
 
     my_app.run(data=data, state=state)
 
 
-#@TODO: progress bar пропал после обновления страницы и снова появилась кнопка
+# @TODO: progress bar пропал после обновления страницы и снова появилась кнопка
 if __name__ == "__main__":
     sly.main_wrapper("main", main)
