a3f3b2911a5ff2b92a16eb1665e87b51013b75d8
cxnt <pavelbartsits@gmail.com>
2023-04-27 20:12:12 +0400

    add checkbox to always use model suffix

--- shared_utils/inference.py
+++ shared_utils/inference.py
@@ -3,13 +3,19 @@ import shared_utils.ui2 as ui
 from shared_utils.merge_metas import merge_metas
 
 
-def postprocess(api: sly.Api, project_id, ann: sly.Annotation,
-                project_meta: sly.ProjectMeta, model_meta: sly.ProjectMeta,
-                state):
+def postprocess(
+    api: sly.Api,
+    project_id,
+    ann: sly.Annotation,
+    project_meta: sly.ProjectMeta,
+    model_meta: sly.ProjectMeta,
+    state,
+):
     keep_classes = ui.get_keep_classes(state)  # @TODO: for debug ['dog'] #
     keep_tags = ui.get_keep_tags(state)
-    res_project_meta, class_mapping, tag_meta_mapping = \
-        merge_metas(project_meta, model_meta, keep_classes, keep_tags, state["suffix"])
+    res_project_meta, class_mapping, tag_meta_mapping = merge_metas(
+        project_meta, model_meta, keep_classes, keep_tags, state["suffix"], state["useModelSuffix"]
+    )
 
     image_tags = []
     for tag in ann.img_tags:
@@ -26,7 +32,9 @@ def postprocess(api: sly.Api, project_id, ann: sly.Annotation,
             if tag.meta.name not in keep_tags:
                 continue
             label_tags.append(tag.clone(meta=tag_meta_mapping[tag.meta.name]))
-        new_label = label.clone(obj_class=class_mapping[label.obj_class.name], tags=sly.TagCollection(label_tags))
+        new_label = label.clone(
+            obj_class=class_mapping[label.obj_class.name], tags=sly.TagCollection(label_tags)
+        )
         new_labels.append(new_label)
 
     res_ann = ann.clone(labels=new_labels, img_tags=sly.TagCollection(image_tags))
