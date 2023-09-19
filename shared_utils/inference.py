7c5dc7c5a59870011f5c437cbd2e30e42176b86b
almaz <almazgimaev@gmail.com>
2023-09-19 11:19:54 +0300

    remove try-except

--- shared_utils/inference.py
+++ shared_utils/inference.py
@@ -11,16 +11,8 @@ def postprocess(
     model_meta: sly.ProjectMeta,
     state,
 ):
-    try:
-        keep_classes = ui.get_keep_classes(state)  # @TODO: for debug ['dog'] #
-        keep_tags = ui.get_keep_tags(state)
-    except KeyError as e:
-        raise Exception(
-            "Can not get classesInfo/tagsInfo from state. "
-            "You are probably trying to use a model trained outside Supervisely. "
-            "You can use one of pretrained models or train a model in Supervisely via corresponding app. "
-            f"{repr(e)}"
-        )
+    keep_classes = ui.get_keep_classes(state)  # @TODO: for debug ['dog'] #
+    keep_tags = ui.get_keep_tags(state)
     res_project_meta, class_mapping, tag_meta_mapping = merge_metas(
         project_meta, model_meta, keep_classes, keep_tags, state["suffix"], state["useModelSuffix"]
     )
