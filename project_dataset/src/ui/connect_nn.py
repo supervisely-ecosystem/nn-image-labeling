9b848f099ab02dea19454cd4f36e83e672d5d587
iwatkot <iwatkot@gmail.com>
2024-02-05 15:49:33 +0300

    UI updates.

--- project_dataset/src/ui/connect_nn.py
+++ project_dataset/src/ui/connect_nn.py
@@ -4,9 +4,9 @@ from supervisely.app.widgets import Button, Card, Container, ModelInfo, SelectAp
 import project_dataset.src.globals as g
 
 select_session = SelectAppSession(g.team_id, g.deployed_nn_tags)
-select_button = Button("Select")
-change_button = Button("Change")
-change_button.hide()
+connect_button = Button("Connect", icon="zmdi zmdi-check")
+disconnect_button = Button("Disconnect", icon="zmdi zmdi-close")
+disconnect_button.hide()
 no_sessions_text = Text(
     "App session was not selected, please select it and press the button again.", status="warning"
 )
@@ -18,16 +18,17 @@ model_info.hide()
 card = Card(
     "2️⃣ Choose model",
     "Select a deployed neural network to start and click on Select button.",
-    content=Container([select_session, select_button, model_info, no_sessions_text]),
-    content_top_right=change_button,
+    content=Container(
+        [select_session, connect_button, model_info, disconnect_button, no_sessions_text]
+    ),
     collapsable=True,
-    lock_message="Click on Change button to select another app session, input data on step 1️⃣ must be selected.",
+    lock_message="Select a project or a dataset on step 1️⃣.",
 )
 card.collapse()
 card.lock()
 
 
-@select_button.click
+@connect_button.click
 def model_selected():
     g.model_session_id = select_session.get_selected_id()
     sly.logger.info(f"Select button was clicked. Model: {g.model_info}")
@@ -37,19 +38,19 @@ def model_selected():
     no_sessions_text.hide()
     model_info.set_session_id(g.model_session_id)
     model_info.show()
-    change_button.show()
+    disconnect_button.show()
 
-    select_button.hide()
+    connect_button.hide()
     select_session.hide()
     # TODO: Unlock next card in UI
 
 
-@change_button.click
+@disconnect_button.click
 def model_changed():
-    select_button.show()
+    connect_button.show()
     select_session.show()
 
-    change_button.hide()
+    disconnect_button.hide()
     g.model_session_id = None
     g.model_meta = None
     sly.logger.info(f"Change button was clicked. Model: {g.model_info}")
