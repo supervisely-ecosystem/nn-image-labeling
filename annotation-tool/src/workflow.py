bcb72609ab502677629f1c8b7ef362a079f0e1f1
GoldenAnpu <seryios.sych@gmail.com>
2024-07-08 12:52:50 +0200

    Refactor Workflow Update SDK verion to v6.73.123

--- annotation-tool/src/workflow.py
+++ annotation-tool/src/workflow.py
@@ -17,18 +17,18 @@ class Workflow:
         self.is_compatible = None
         self.api = api
         self._min_instance_version = (
-            "6.9.22" if min_instance_version is None else min_instance_version
+            "6.9.31" if min_instance_version is None else min_instance_version
         )
 
     @check_compatibility
     def add_input(self, project_id: int, state: dict):
         if self.api.project.get_info_by_id(project_id).version:  # to prevent cycled workflow
-            self.api.app.add_input_project(project_id)
-        self.api.app.add_input_task(int(state["sessionId"]))
+            self.api.app.workflow.add_input_project(project_id)
+        self.api.app.workflow.add_input_task(int(state["sessionId"]))
 
     @check_compatibility
     def add_output(self, project_id: int):
-        self.api.app.add_output_project(project_id)
+        self.api.app.workflow.add_output_project(project_id)
 
     def check_instance_ver_compatibility(self):
         if self.api.instance_version < self._min_instance_version:
