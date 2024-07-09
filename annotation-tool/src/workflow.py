d918a09d1eb553722809d3bc0487a288ddd8d5e0
GoldenAnpu <seryios.sych@gmail.com>
2024-07-10 00:15:17 +0200

    Fix check instance version compatibility

--- annotation-tool/src/workflow.py
+++ annotation-tool/src/workflow.py
@@ -20,6 +20,18 @@ class Workflow:
             "6.9.31" if min_instance_version is None else min_instance_version
         )
 
+    def check_instance_ver_compatibility(self):
+        if not self.api.is_version_supported(self._min_instance_version):
+            sly.logger.info(
+                f"Supervisely instance version {self.api.instance_version} does not support workflow features."
+            )
+            if not sly.is_community():
+                sly.logger.info(
+                    f"To use them, please update your instance to version {self._min_instance_version} or higher."
+                )
+            return False
+        return True
+
     @check_compatibility
     def add_input(self, project_id: int, state: dict):
         if self.api.project.get_info_by_id(project_id).version:  # to prevent cycled workflow
@@ -29,11 +41,3 @@ class Workflow:
     @check_compatibility
     def add_output(self, project_id: int):
         self.api.app.workflow.add_output_project(project_id)
-
-    def check_instance_ver_compatibility(self):
-        if self.api.instance_version < self._min_instance_version:
-            sly.logger.info(
-                f"Supervisely instance version does not support workflow features. To use them, please update your instance minimum to version {self._min_instance_version}."
-            )
-            return False
-        return True
